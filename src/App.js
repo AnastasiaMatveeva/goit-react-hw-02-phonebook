98252c79e556098c113aba4e7e97a37779caaf39
unknown <anastasiagieieva@gmail.com>
2021-07-27 07:58:51 +0300

    added phonebook hw

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter';
+import { v4 as uuidv4 } from 'uuid';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  changeInput = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+  createContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const isContactExist = contacts.find(contact => contact.name === name);
+    if (isContactExist) {
+      alert(`${name} is already axist in contacts`);
+      return;
+    }
+    const id = uuidv4();
+    const newContact = {
+      name,
+      number,
+      id: id,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const normilizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normilizedFilter),
+    );
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.createContact} />
+        <div>
+          <h1>Contacts</h1>
+          <Filter value={this.filter} onChangeInput={this.changeInput} />
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
